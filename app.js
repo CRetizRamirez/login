8fd4e70c1076ecf4ef2a5a89f45cb917134c6509
CRetizRamirez <cretiz.dsr@gmail.com>
2024-12-01 15:13:52 -0600

    Corregi las url en el front

--- app.js
+++ app.js
@@ -8,15 +8,16 @@ import { verifyToken } from "./middlewares/authMiddleware.js";
 
 dotenv.config();
 
+// Define __dirname en modo ES Modules, para leer los archivos estaticos de React
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 
 app.use(cors());
 app.use(express.json())
 app.use("/api/protected", verifyToken);
 app.use('/api', routes); 
- // Define __dirname en modo ES Modules, para leer los archivos estaticos de React
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, 'dist')));  // Middleware para servir los archivos estáticos de React
 
 // Iniciar el servidor
