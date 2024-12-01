547a5d09c70f67712fb494446fe225c4e3e35f14
CRetizRamirez <cretiz.dsr@gmail.com>
2024-12-01 11:56:05 -0600

    Quite la parte Front prara subir solo la API

--- app.js
+++ app.js
@@ -8,17 +8,16 @@ import { verifyToken } from "./middlewares/authMiddleware.js";
 
 dotenv.config();
 
-// Define __dirname en modo ES Modules, para leer los archivos estaticos de React
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 const app = express();
 
 app.use(cors());
 app.use(express.json())
 app.use("/api/protected", verifyToken);
 app.use('/api', routes); 
-app.use(express.static(path.join(__dirname, 'dist')));  // Middleware para servir los archivos estáticos de React
+// // Define __dirname en modo ES Modules, para leer los archivos estaticos de React
+//const __filename = fileURLToPath(import.meta.url);
+//const __dirname = path.dirname(__filename);
+//app.use(express.static(path.join(__dirname, 'dist')));  // Middleware para servir los archivos estáticos de React
 
 // Iniciar el servidor
 const SERVER_PORT = process.env.SERVER_PORT || 3000;
@@ -26,5 +25,5 @@ const DB_HOST = process.env.DB_HOST;
 
 app.listen(SERVER_PORT, () => {
     console.log(`Servidor corriendo en el puerto: ${SERVER_PORT}`);
-    console.log(`Dominio: ${DB_HOST}`);
+    console.log(`Host de la DB: ${DB_HOST}`);
 });
