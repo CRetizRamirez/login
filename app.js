844a42aab1b793cdce4a19bfe18fd85bfaa7b1c2
CRetizRamirez <cretiz.dsr@gmail.com>
2024-12-01 12:13:22 -0600

    Subi el Front y la version de Node

--- app.js
+++ app.js
@@ -14,10 +14,10 @@ app.use(cors());
 app.use(express.json())
 app.use("/api/protected", verifyToken);
 app.use('/api', routes); 
-// // Define __dirname en modo ES Modules, para leer los archivos estaticos de React
-//const __filename = fileURLToPath(import.meta.url);
-//const __dirname = path.dirname(__filename);
-//app.use(express.static(path.join(__dirname, 'dist')));  // Middleware para servir los archivos estáticos de React
+ // Define __dirname en modo ES Modules, para leer los archivos estaticos de React
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname, 'dist')));  // Middleware para servir los archivos estáticos de React
 
 // Iniciar el servidor
 const SERVER_PORT = process.env.SERVER_PORT || 3000;
