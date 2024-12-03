c3dc5cfc38b055ee090327cbd05fcbe713d68a06
CRetizRamirez <cretiz.dsr@gmail.com>
2024-12-03 00:02:47 -0600

    No era necesario escribir aqui el protected

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ import cors from 'cors'
 import path from 'path';
 import { fileURLToPath } from 'url';
 import routes from './routes/Routes.js'
-import { verifyToken } from "./middlewares/authMiddleware.js";
 
 dotenv.config();
 
@@ -12,7 +11,6 @@ const app = express();
 
 app.use(cors());
 app.use(express.json())
-app.use("/api/protected", verifyToken);
 app.use('/api', routes); 
 
 // ***** Para que pueda trabajar con la carpeta dist de React *****
